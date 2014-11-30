c8db7226af9edbe27386f2f1d2d68e12dd9cd685
spitz6860 <spitz6860@live.com>
2014-11-29 18:00:00 -0800

    got it working finally

--- wheel.js
+++ wheel.js
@@ -2,78 +2,12 @@
 function cMod(theta) {
 	return theta % (Math.PI * 2);
 }
-/*
-
-function Emitter(options) {
-	
-	var defaults = {
-		interval: .5,
-		theta: 0,
-		sprites: [],
-		//maxCount: 6,
-		position: pt(0, $(window).height() + 100),
-		height: 600,
-		
-		maxw: 1000,
-		
-		timer: 0,
-	};
-		
-	var e = $.extend({}, defaults, options);
-	for(x in e) this[x] = e[x];
-	
-	this.render = function(ctx) {
-		this.maxw = ctx.canvas.width;
-		this.position.y = ctx.canvas.height + 50;
-		
-		ctx.save();
-		ctx.translate(this.position.x, this.position.y);
-		this.sprites.map(function(q) { q.render(ctx) });
-		ctx.restore();
-	}
-		
-	this.frameMove = function(te) {
-		this.theta += 0.8 * te;
-		this.theta = this.theta % (Math.PI * 2);
-		
-		this.sprites.map(function(q) { q.frameMove(te) });
-		
-		this.sprites.filter(function(q) { return q.position.y > -100; });
-		
-		this.timer += te;
-		
-		if(this.timer >= this.interval) {
-			this.spawn();
-			this.timer = 0;
-		};
-	}
-	
-	
-	this.spawn = function() {
-		console.log('spawned');
-		this.sprites.push(new Heart({
-			position: pt(Math.random() * this.maxw, 0),
-			size: .2 + Math.random() * .2,
-			phase: Math.random() * 3,
-			
-		}));
-	}
-	
-	this.init = function(te) {
-		
-		
-	}
-	
-	this.init();
-}
-*/
-
 
 function Wheel(options) {
 	
 	var defaults = {
-		insideRadius: 80,
-		outsideRadius: 325,
+		insideRadius: 35,
+		outsideRadius: 300,
 		position: pt(330,330),
 		theta: 0,
 		angularVelocity: 0,
@@ -83,17 +17,15 @@ function Wheel(options) {
 		settling: false,
 		
 		slices: [
-			{name: 'Shantoodle', size: 30 },
-			{name: 'Subway', size: 40 },
-			{name: 'Bai Tong', size: 50 },
-			{name: 'Indian', size: 60 },
 		],
 		
 		totalSize: 1,
 		
 		colors: [
-			'#a7a9ac',
-			'#6d6e71',
+			'red',
+			'yellow',
+			'green',
+			'blue'
 		],
 	};
 		
@@ -157,7 +89,7 @@ function Wheel(options) {
 			var textwidth = metrics.width;
 			
 			if(textwidth > this.outsideRadius - this.insideRadius - 24)
-				ctx.font = '14px Arial';
+				ctx.font = '16px Arial';
 			
 			metrics = ctx.measureText(this.slices[i].name);
 			textwidth = metrics.width;
@@ -165,8 +97,8 @@ function Wheel(options) {
 				ctx.font = '12px Arial';
 			
 			
-			ctx.fillStyle = '#bdbec0';
-			var tx = this.insideRadius + 12;
+			ctx.fillStyle = 'silver';
+			var tx = this.insideRadius + 30;
 			var ty = 10;
 			ctx.fillText(this.slices[i].name, tx, ty);
 			
@@ -185,7 +117,7 @@ function Wheel(options) {
 			ctx.closePath();
 			
 			ctx.lineWidth = 3;
-			ctx.strokeStyle = '#bdbec0';
+			ctx.strokeStyle = 'black';
 			ctx.stroke();
 			
 			ctx.rotate(rads);
@@ -196,15 +128,15 @@ function Wheel(options) {
 		// draw circles
 		
 		ctx.beginPath();
-		ctx.lineWidth = 12;
+		ctx.lineWidth = 10;
 		ctx.arc(0, 0,  this.insideRadius, 0, Math.PI * 2, false);
-		ctx.strokeStyle = '#f7941e';
+		ctx.strokeStyle = 'black';
 		ctx.stroke();
 		
 		ctx.beginPath();
-		ctx.lineWidth = 10;
+		ctx.lineWidth = 20;
 		ctx.arc(0, 0,  this.outsideRadius, 0, Math.PI * 2, false);
-		ctx.strokeStyle = '#f7941e';
+		ctx.strokeStyle = 'black';
 		ctx.stroke();
 		
 	
@@ -299,10 +231,7 @@ function Wheel(options) {
 	this.init = function() {
 		
 		this.totalSize = this.slices.reduce(function(acc, q) { return acc + q.size; }, 0);
-		
-		this.spin();
-		
-		
+			
 	}
 	
 	this.init();
