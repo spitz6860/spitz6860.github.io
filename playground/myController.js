9ef3ac978f7146859451bb497b3285721fdb86cd
spitz6860 <spitz6860@live.com>
2015-03-08 21:35:41 -0700

    something

--- playground/myController.js
+++ playground/myController.js
@@ -1,11 +1,44 @@
-var demoApp = angular.module('demoApp', []);
+var myDirectives = angular.module('myDirectives', []);
 
 
-demoApp.controller('MyController', function ($scope) {
-	$scope.names = [
-		{name: 'one', age: 20},
-		{name: 'two', age: 22},
-		{name: 'three', age: 23},
-		{name: 'four', age: 25}
-	]
+myDirectives.directive('mySearchbox', function () {
+	return {
+		restrict: 'E',
+		scope: {
+			searchText: '=',
+			placeholder: '@',
+			usedLucky: '='
+		},
+		template: 
+			'<div>' + 
+			'	<input type="text" ng-model="tempSearchText" placeholder="{{placeholder}}">' +
+			'	<button ng-click="searchClicked()">' +
+			'		Search' +
+			'	</button>' +
+			'	<button ng-click="luckyClicked()">' +
+			'		I\'m feeling lucky' +
+			'	</button>' +
+			'</div>',
+		link: function(scope, element, attrs) {
+			
+			scope.searchClicked = function() {
+				scope.searchText = scope.tempSearchText;
+				scope.usedLucky = false;
+
+				console.log(scope);
+				
+			}
+			scope.luckyClicked = function() {
+				scope.searchText = scope.tempSearchText;
+				scope.usedLucky = true;
+				console.log(scope);
+			}
+		}
+	}
 });
+
+
+var demoApp = angular.module('demoApp', ['myDirectives']);
+
+var something = null;
+console.log(something);
