0d1ee859d8c33e346803edcd07436f83e54062f9
spitz6860 <spitz6860@live.com>
2015-03-19 21:13:10 -0700

    more quotes!

--- js/quotes.js
+++ js/quotes.js
@@ -17,6 +17,38 @@
 		{
 			text: "If you have time to think of a beautiful ending, why not use the time to live beautifully until the end.",
 			source: 'Gintama'
+		},
+		{
+			text: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+			source: 'Bernard Baruch'
+		},
+		{
+			text: "You only live once, but if you do it right, once is enough.",
+			source: 'Mae West'
+		},
+		{
+			text: "If you tell the truth, you don't have to remember anything.",
+			source: 'Mark Twain'
+		},
+		{
+			text: "It is better to be hated for what you are than to be loved for what you are not.",
+			source: "Autumn Leaves"
+		},
+		{
+			text: "I have not failed. I've just found 10,000 ways that won't work.",
+			source: "Thomas Edison"
+		},
+		{
+			text: "Life is like riding a bicycle. To keep your balance, you must keep moving."
+			source: "Albert Einstein"
+		},
+		{
+			text: "Anyone who has never made a mistake has never tried anything new.",
+			source: "Albert Einstein"
+		},
+		{
+			text: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+			source: "Albert Einstein"
 		}
 	];
 
