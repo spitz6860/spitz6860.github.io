abdc4682811cf7a2a44210198bd83c7bc7bd8203
kai <kai@atomicinfotech.com>
2015-04-14 22:27:16 -0700

    test and new files

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -32,11 +32,13 @@ var LinkedList = (function () {
 		if (this.head) {
 			if (this.head.data === val) {
 				this.head = this.head.next;
+				this.length--;
 			} else {
 				current = this.head;
 				while(current && current.next) {
 					if (current.next.data === val) {
 						current.next = current.next.next;
+						this.length--;
 					}
 					current = current.next;
 				}
@@ -65,3 +67,24 @@ var LinkedList = (function () {
 
 	return LinkedList;
 })();
+
+testLinkedList();
+
+function testLinkedList() {
+	var list = new LinkedList();
+	console.log("var list = new LinkedList();\n" + list.toString() + "\nlength: " + list.getLength());
+	list.append('5');
+	console.log("list.append('5');\n" + list.toString() + "\nlength: " + list.getLength());
+	list.append(5);
+	console.log("list.append('5');\n" + list.toString() + "\nlength: " + list.getLength());
+	list.append('item');
+	console.log("list.append('item');\n" + list.toString() + "\nlength: " + list.getLength());
+	list.append(true);
+	console.log("list.append(true);\n" + list.toString() + "\nlength: " + list.getLength());
+	list.remove('item');
+	console.log("list.remove('item');\n" + list.toString() + "\nlength: " + list.getLength());
+	list.remove('true');
+	console.log("list.remove('true');\n" + list.toString() + "\nlength: " + list.getLength());
+	list.remove('5');
+	console.log("list.remove('5');\n" + list.toString() + "\nlength: " + list.getLength());
+}
