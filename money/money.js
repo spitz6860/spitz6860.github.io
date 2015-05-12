48e15a1ef6459f7da000e839852a2d99c315c2ed
spitz6860 <spitz6860@live.com>
2015-05-11 22:51:53 -0700

    random stuff

---
+++ money/money.js
@@ -0,0 +1,28 @@
+var stockApp = angular.module('StockApp', ['ui.bootstrap']);
+
+
+stockApp.controller('StockController', function($scope, $http) {
+	var tickers = ['BAC', 'CPA', 'DDD', 'LINE', 'MOBI', 'NOK'];
+	
+	getData(tickers);
+	function getData(ticker) {
+		if (ticker.length > 0) {
+			var queryString = ticker.join(',');
+		
+		 	var stockRequest =  $http.jsonp("https://query.yahooapis.com/v1/public/yql", 
+		 		{
+		 			params: {
+		 				q: "select * from yahoo.finance.quotes where symbol in ('" + queryString + "') ",
+		 				format: "json",
+		 				env: "http://datatables.org/alltables.env",
+		 				callback: "JSON_CALLBACK"
+		 			}
+		 		});
+		 	stockRequest.success(function(data) {
+		 		$scope.mystock = data.query.results.quote;
+		 	});
+	 	} else {
+	 		$scope.error = 'Invalid param for getData function';
+	 	}
+	}
+});
